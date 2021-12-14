d7ae03954ee4a00bb8fd0d77dd59563a24fdc74b
Robert Mihai Nicolescu <robert.nicolescu1992@gmail.com>
2021-12-14 13:42:52 +0200

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,55 @@
+
+logo = '''
+____________
+| ________ |
+||12345678||
+|""""""""""|
+|[M|#|C][-]|
+|[7|8|9][+]|
+|[4|5|6][x]|
+|[1|2|3][%]|
+|[.|O|:][=]|
+"----------" '''
+
+print(logo)
+#Now we create the functions
+def add (n1, n2):
+    return n1 + n2
+
+def substraction (n1, n2):
+    return n1 - n2
+
+def multiply (n1, n2):
+    return n1 * n2
+
+def divide (n1, n2):
+    return n1 / n2
+
+def modulo (n1, n2):
+    return n1 % n2
+
+def power (n1, n2):
+    return n1 ** n2
+
+def calculator():
+    num1 = float(input("What's the first number: \n"))
+    operations = {"+": add, "-": substraction, "*": multiply, "/": divide, "%": modulo, "**": power}
+    for key in operations:
+        print(key)
+    should_continue = True
+    while should_continue:
+
+        operation_symbol = input("Pick operation: ")
+        num2 = float(input("What's the next number: \n"))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input("Type 'y' to continue or 'n' to exit: ") == 'y'.lower():
+            num1 is answer
+        else:
+            should_continue = False
+            calculator()
+
+calculator()
